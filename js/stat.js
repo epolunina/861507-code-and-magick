ecf3cb0e25568ef14a2751391ffba7e3335ddf56
epolunina <e.polunina@qiwi.ru>
2018-09-12 12:33:13 +0300

    Исправила ошибки

--- js/stat.js
+++ js/stat.js
@@ -6,12 +6,16 @@ var CLOUD_Y = 10;
 var CLOUD_HEIGHT = 270;
 var CLOUD_WIDTH = 420;
 var GAP = 10;
+// промежутки  между текстом и гистограммой
+var INTERVAL = 20;
 // ширина колонки - 40px
 var BAR_WIDTH = 40;
 // расстояние между колонками
 var BAR_GAP = 50;
 // высота гистограммы  150px
 var BAR_HEIGHT = 150;
+// координата облачка снизу
+var CLOUD_Y_LOW = CLOUD_HEIGHT - CLOUD_Y;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -31,24 +35,24 @@ var getMaxElement = function (arr) {
 var drawBar = function (ctx, names, times, i, maxTime) {
   ctx.fillText(
       names,
-      CLOUD_X + 2 * GAP + i * (BAR_GAP + BAR_WIDTH),
-      CLOUD_HEIGHT - CLOUD_Y
+      CLOUD_X + INTERVAL + i * (BAR_GAP + BAR_WIDTH),
+      CLOUD_Y_LOW
   );
   ctx.fillRect(
-      CLOUD_X + 2 * GAP + i * (BAR_GAP + BAR_WIDTH),
-      CLOUD_HEIGHT - CLOUD_Y - 2 * GAP - (BAR_HEIGHT * times) / maxTime,
+      CLOUD_X + INTERVAL + i * (BAR_GAP + BAR_WIDTH),
+      CLOUD_Y_LOW - INTERVAL - (BAR_HEIGHT * times) / maxTime,
       BAR_WIDTH,
       (BAR_HEIGHT * times) / maxTime
   );
   ctx.fillText(
       Math.round(times),
-      CLOUD_X + 2 * GAP + i * (BAR_GAP + BAR_WIDTH),
-      CLOUD_HEIGHT - CLOUD_Y - 3 * GAP - (BAR_HEIGHT * times) / maxTime
+      CLOUD_X + INTERVAL + i * (BAR_GAP + BAR_WIDTH),
+      CLOUD_Y_LOW - 3 * GAP - (BAR_HEIGHT * times) / maxTime
   );
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba (0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.font = 'bold 16px PT Mono';
@@ -57,12 +61,10 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Список результатов: ', CLOUD_X + GAP, CLOUD_Y + 5 * GAP);
   var maxTime = getMaxElement(times);
   for (var i = 0; i < names.length; i++) {
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      drawBar(ctx, names[i], times[i], i, maxTime);
-    } else {
-      ctx.fillStyle = 'hsl(240,' + Math.random() * 100 + '% ,50%)';
-      drawBar(ctx, names[i], times[i], i, maxTime);
-    }
+    ctx.fillStyle =
+      names[i] === 'Вы'
+        ? 'rgba(255, 0, 0, 1)'
+        : 'hsl(240,' + Math.random() * 100 + '% ,50%)';
+    drawBar(ctx, names[i], times[i], i, maxTime);
   }
 };
